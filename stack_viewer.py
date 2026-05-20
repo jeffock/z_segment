ad945ddc64716b08f358260197e724d338f4f603
jeffock <jeffock1656@gmail.com>
2026-05-20 00:09:18 -0500

    remove stack_viewer.py

--- stack_viewer.py
+++
@@ -1,7 +0,0 @@
-# Z-STACK VIEWER
-# 05/19/26
-# Jeffrey Ock
-# Dataset: https://www.kaggle.com/datasets/burakkahveci/brain-organoids-segmentation-dataset?resource=download
-
-img_path = "dataset/Segmentation/BO-WoAugmentation/img/"
-seg_path = "dataset/Segmentation/BO-WoAugmentation/seg/"
