5713151c36cc26621b7de807f6c2765ecf3f76e2
jeffock <jeffock1656@gmail.com>
2026-05-20 00:43:04 -0500

    CMake TEST

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
-# include "view_zstack.h"
+// MAINLOOP
+// Jeffrey Ock
+// 05/20/26
+
+
+//---------- HEADERS ----------//
+#include "view_zstack.h"
+#include <iostream>
 
 int main()
 {
+	std::cout << "TEST" << std::endl;
+	return 0;
 }
